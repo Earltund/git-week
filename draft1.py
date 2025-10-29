49b88fcbc73334d71d420c48aed4976372d78637
Michael Okunade <y5v76@mercia.keele.ac.uk>
2025-10-29 14:10:00 +0000

    first commit

---
+++ draft1.py
@@ -0,0 +1,7 @@
+print('''Welcome to Library Map text Appplication \n 
+      Select one of the following options: \n 
+      1. Enter the subject name or part-name \n
+      2. Enter the subject classmark (CM) \n
+      3. Enter the location of the subject that you are trying to find. 
+      ''')
+option = int(input('Select from one of the following options above (1, 2, or 3): '))
