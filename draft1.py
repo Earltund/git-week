d0a67cf2e7f7eae5a373db38866548f782a90d75
Earltund <michealayotunde71@gmail.com>
2025-10-29 15:07:56 +0000

    Revert "second commit"
    This reverts commit 640a7cfad05d26467991a0bf139ae66e896239b3.

--- draft1.py
+++ draft1.py
@@ -5,7 +5,3 @@ print('''Welcome to Library Map text Appplication \n
       3. Enter the location of the subject that you are trying to find. 
       ''')
 option = int(input('Select from one of the following options above (1, 2, or 3): '))
-
-
-if option == 1:
-    print("1 commit works")
